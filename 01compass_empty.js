4c30461edda20f1f85c0dcb40e405e65dfd3d42a
IlennaK <kanninen.anneli@gmail.com>
2018-02-22 12:16:12 +0200

    Initial commit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,7 +8,7 @@ oriented = function(e) {
             compassHeading = e.alpha;
         }
         if(compassHeading > 315 || compassHeading < 45) {
-                text_area.textContent = ";
+                text_area.textContent = "sun";
         }
 
         if(compassHeading >= 45 && compassHeading < 135) {
